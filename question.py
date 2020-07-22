cd4fe4d274b920f6d2ecd269ebb16fda188f28d2
predictorteam03 <58499211+predictorteam03@users.noreply.github.com>
2020-07-22 22:09:37 +0530

    Add files via upload

---
+++ question.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+
+
+
+# # Question 1
+# n=10
+# sum1 = 0
+# while(n > 0):
+#     sum1=sum1+n
+#     n=n-1
+# print("The sum of first 10 natural numbers is",sum1)
+
+# In[ ]:
+
+
+
+
+
+# # Question 2
+# number = int(input("Enter any number: "))
+# 
+# if number > 1:
+#     for i in range(2, number):
+#         if (number % i) == 0:
+#             print(number, "is not a prime number")
+#             break
+#     else:
+#         print(number, "is a prime number")
+# 
+# else:
+#     print(number, "is not a prime number")
+
+# In[ ]:
+
+
+
+
